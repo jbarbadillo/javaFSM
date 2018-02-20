5c8873429076194e398e4d18bc77d152e6045495
jbarbadillo <javier.barbadillo@gmail.com>
2018-02-20 22:05:25 +0100

    Create State inner class

--- src/main/java/StateMachine.java
+++ src/main/java/StateMachine.java
@@ -1,3 +1,13 @@
 public class StateMachine {
-	
+	public class State{
+		String name;
+		Map<String, State> transitionMap;
+		Runnable function = null;
+		
+		State(final String name){
+			this.name = name;
+			transitionMap = new HashMap<>();
+		}
+	};
+	StateMachine();
 }
