e563eb09661d4055c15c9c14e852f67ae3d2d5f3
jbarbadillo <javier.barbadillo@gmail.com>
2018-02-23 18:01:03 +0100

    Add integration test

---
+++ src/test/java/IntegrationTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+public class StateMachineIntegrationTest {
+}
